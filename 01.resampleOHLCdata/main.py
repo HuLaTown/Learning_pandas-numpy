55759ee482df8a94b3be4e4cd11b15c916a52b77
HuLaTown <hucifer@live.cn>
2016-05-25 00:09:09 +0800

    TODO divide&merge

--- 01.resampleOHLCdata/main.py
+++ 01.resampleOHLCdata/main.py
@@ -3,6 +3,8 @@ import pandas as pd
 
 df = pd.read_table('000002.txt', names=['tradedate', 'minute', 'high', 'low', 'close', 'volume'])
 
+df.insert(2, 'open', df.close.shift(1))  # copy OPEN by the previous CLOSE
+
 df.loc[:, 'tradedate'] = [str(x) for x in df.loc[:, 'tradedate']]
 df.loc[:, 'minute'] = ['0'+str(x) if len(str(x)) < 4 else str(x) for x in df.loc[:, 'minute']]
 df.insert(0, 'datetime', df.tradedate+df.minute)
@@ -11,20 +13,37 @@ df.loc[:, 'datetime'] = [datetime.strptime(x, '%Y%m%d%H%M') for x in df.loc[:, '
 
 df = df.drop(df.columns[[1, 2]], 1)
 
-df.insert(1, 'open', df.close)  # insert test data of open price since real open price data is missing
+
+# df.insert(1, 'open', df.close)  # insert test data of open price since real open price data is missing
 # need to be removed after open price added
 
+
 ohlc_dict = {
-    'Open': 'first',
-    'High': 'max',
-    'Low': 'min',
-    'Close': 'last',
-    'Volume': 'sum'
+    'open': 'first',
+    'high': 'max',
+    'low': 'min',
+    'close': 'last',
+    'volume': 'sum'
     }
 
 df = df.set_index(['datetime'])
 
+
+# TODO get all trade dates
+# pd.PeriodIndex(df.index,freq='D').unique()
+
+# TODO foreach trade date, resample am & pm data according to the bin size
+# df[df.index < datetime(tradedates[0].strftime('%Y'), tradedates[0].strftime('%m'), tradedates[0].strftime('%d'),11,30,00)]
+# y =
+
+
+# TODO merge them all(append)
+
+
 df.resample('5min', how=ohlc_dict, label='right', closed='right')
+
+# bin_size = 5
+
 
 # TODO prevent NAN data beyond the trading time
 # TODO shift the resampled data in order to show the price of 10:00
